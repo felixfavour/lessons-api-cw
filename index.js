c0fc3a1ccb7aa2474acb88d8c9400ff982cf482a
Favour Felix <felixfavour0@gmail.com>
2022-02-14 00:52:12 +0400

    changed the server port to allow process env

--- index.js
+++ index.js
@@ -3,22 +3,46 @@ var cors = require("cors");
 
 var http = require("http");
 var app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 var lessons = require("./data/lessons.json");
 var user = require("./data/user.json");
+app.use(express.json())
 
 //CORS allows you to configure the web API's security. It has to do with allowing other domains to make requests against your web API.
 app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+const { MongoClient } = require('mongodb')
+const username = 'favourfelix'
+const password = 'emmanuel'
+const dbName = 'afterschool'
+const collectionName = 'lessons'
 
-app.get("/lessons", (req, res) => {
+let db = null
+
+async function main () {
+  const client = new MongoClient(`mongodb+srv://${username}:${password}@cluster0.5g703.mongodb.net/${dbName}?retryWrites=true&w=majority`)
+  try {
+    await client.connect()
+    db = client.db(dbName)
+    console.log('connected')
+  }
+  catch(err) {
+    console.error('ERR', err)
+  }
+}
+
+main().catch(console.error)
+
+app.get("/lessons", async (req, res) => {
+  const lessons = await db.collection(collectionName).find().toArray()
   res.status(200).json(lessons); 
 });
 
 app.get("/user", (req, res) => {
-  res.status(200).json(user); 
+  res.status(200).json({}); 
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
